201efdb5d44f929a6b5d027eaacc72f47aebe848
rupali64 <72986967+rupali64@users.noreply.github.com>
2024-10-24 12:49:19 +0530

    Add files via upload

---
+++ Finance Analytics.sql
@@ -0,0 +1,15 @@
+SELECT * FROM dim_customer WHERE customer like "%croma%" AND market="india";
+
+SELECT * FROM fact_sales_monthly 
+	WHERE 
+            customer_code=90002002 AND
+            YEAR(DATE_ADD(date, INTERVAL 4 MONTH))=2021 
+	ORDER BY date asc
+	LIMIT 100000;
+    
+    SELECT * FROM fact_sales_monthly 
+	WHERE 
+            customer_code=90002002 AND
+            get_fiscal_year(date)=2021 
+	ORDER BY date asc
+	LIMIT 100000;
